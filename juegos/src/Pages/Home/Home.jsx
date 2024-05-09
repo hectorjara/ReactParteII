714cfdde742b14edd6c59300e9599523499b0865
hectorjara <hector.jara@est.fi.uncoma.edu.ar>
2024-05-09 05:39:57 -0300

    Navegacion por parametros

--- juegos/src/Pages/Home/Home.jsx
+++ juegos/src/Pages/Home/Home.jsx
@@ -1,12 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import { ROUTES } from "../../Const/routes";
 import { useState, useEffect } from "react";
 import Footer from "../../Components/Footer/Footer";
 import Header from "../../Components/Header/Header";
 function Home() {
     const [juegos, setJuegos] = useState([]);
+    const navegar = useNavigate();
+
     const fetchJuegos = async () =>{
         const response = await fetch("/mocks/juegos.json");
         const result = await response.json();
         setJuegos(result);
+    };
+
+    function handleClick(id){
+        navegar(ROUTES.detalles.slice(0,-3)+id); //Esto solo quita ":id"  de "detalles:id" y añade el id
     };
 
     useEffect(() => {
@@ -21,7 +29,7 @@ function Home() {
             {juegos.map(juego =>{
                 return(
                     <li key={juego.id}>
-                        <h1>{juego.titulo}</h1>
+                        <h1 onClick={()=>handleClick(juego.id)}>{juego.titulo}</h1>
                     </li>)
             })}
         </ul>
