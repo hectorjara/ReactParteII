bd04f43b61173a2cadaf7ad170e68bf1e5cddecb
hectorjara <hector.jara@est.fi.uncoma.edu.ar>
2024-05-09 04:11:21 -0300

    Llamada al json

--- juegos/src/Pages/Home/Home.jsx
+++ juegos/src/Pages/Home/Home.jsx
@@ -1,12 +1,33 @@
+import { useState, useEffect } from "react";
 import Footer from "../../Components/Footer/Footer";
 import Header from "../../Components/Header/Header";
 function Home() {
-  return (
-    <div className="Home">
-      <Header/>
-      <Footer/>
-    </div>
-  );
+    const [juegos, setJuegos] = useState([]);
+    const fetchJuegos = async () =>{
+        const response = await fetch("/mocks/juegos.json");
+        const result = await response.json();
+        setJuegos(result);
+    };
+
+    useEffect(() => {
+        fetchJuegos()
+        },
+    []);
+
+    return (
+        <div className="Home">
+        <Header/>
+        <ul>
+            {juegos.map(juego =>{
+                return(
+                    <li key={juego.id}>
+                        <h1>{juego.titulo}</h1>
+                    </li>)
+            })}
+        </ul>
+        <Footer/>
+        </div>
+    )
 }
 
 export default Home;
